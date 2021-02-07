f41b5f6330ac21443a1374ca32affb4c79490d24
Potato1682 <contact@potato1682.ml>
2021-02-07 17:00:16 +0900

    Allow CORS

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 
-const
-    app = express(),
-    server = app.listen(8080, () => {
-        console.log("Server listening on", 8080);
-    });
+const app = express();
+
+app.use((_, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get("/", (_, response) => {
     response.send("<p>Hello, world!</p>");
+});
+
+app.listen(8080, () => {
+    console.log("Server listening on", 8080);
 });
 
